d02567c215ed6a614a81b550d86fde7b1ee46c15
BuildTools <unconfigured@null.spigotmc.org>
2021-09-04 12:24:36 +0200

    Obligatorisk innlevering

---
+++ Programmering uke 36/src/no/hvl/dat100/O2.java
@@ -0,0 +1,54 @@
+package no.hvl.dat100;
+
+import static java.lang.Integer.parseInt;
+import static javax.swing.JOptionPane.*;
+
+public class O2 {
+
+	public static void main(String[] args) {
+		int poeng;
+		
+		for (int i = 0; i < 10; i ++) {
+			
+			while (true) {
+				String poengTxt = showInputDialog("Antall poeng:");
+				poeng = parseInt(poengTxt);
+				
+				if (poeng >= 0 && poeng <= 100) {
+					break;
+				}
+				else {
+					showMessageDialog(null,"Ugyldig poengsum");
+				}
+			}
+			
+			
+			String karakter = "";
+			
+				if (poeng <= 39 ) {
+					karakter = "F";
+				}
+				else if (poeng <= 49) {
+					karakter = "E";
+				}
+				else if (poeng <= 59) {
+					karakter = "D";
+				}
+				else if (poeng <= 79) {
+					karakter = "C";
+				}
+				else if (poeng <= 89) {
+					karakter = "B";
+				}
+				else if (poeng <= 100) {
+					karakter = "A";
+				}
+				
+				System.out.println(karakter);
+				showMessageDialog(null,"Elevens karakter er: " + karakter);
+		}
+		
+
+	}
+
+}
